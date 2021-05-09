c1b66091823d765f4628b8bb6be72711e93804ae
JMOrbegoso <dev@jmorbegoso.com>
2021-05-09 12:38:44 -0500

    Created Homework Week-05 main.cpp

---
+++ Homework/Week-05/main.cpp
@@ -0,0 +1,27 @@
+//
+//                                EJERCICIOS DE COLAS
+// En un banco se tiene colas de clientes que llegan a presentar sus expedientes
+// los cuales están ordenados por número y serán atendidos en ese orden.
+// Realizar un programa que permita registrar la cola de clientes y de cada
+// cliente registrar los expedientes que llevará al banco. Registrar persona
+// Registrar expedientes
+// Buscar persona
+// Buscar expediente
+// Listar personas
+// Mostrar expedientes de una persona
+//
+
+#include "menu.h"
+
+using namespace std;
+
+int main() {
+  bankStruct bank;
+
+  collectionsInitialization(bank);
+  dataInitialization(bank);
+
+  mainMenu(bank);
+
+  return 0;
+}
