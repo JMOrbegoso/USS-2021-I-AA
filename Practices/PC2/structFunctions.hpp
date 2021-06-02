7ccff41b3045bc434e1d6a07b09928531ed687bc
JMOrbegoso <dev@jmorbegoso.com>
2021-06-02 17:07:12 -0500

    Created structFunctions.hpp

---
+++ Practices/PC2/structFunctions.hpp
@@ -0,0 +1,9 @@
+#include "helpers.hpp"
+#include "structs.hpp"
+
+using namespace std;
+
+void collectionsInitialization(libraryStruct &library) {
+  library.rooms.head = NULL;
+  library.rooms.length = 0;
+}
