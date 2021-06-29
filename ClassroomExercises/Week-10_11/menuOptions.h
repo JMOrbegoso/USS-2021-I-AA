7cee2188b46568c67c8fd70d3bba979ecf041e1c
JM Orbegoso <dev@jmorbegoso.com>
2021-06-29 09:03:22 -0500

    Created menuOptions.h

---
+++ ClassroomExercises/Week-10_11/menuOptions.h
@@ -0,0 +1,43 @@
+#include "structFunctions.h"
+
+void registerCity(citiesGraph &cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void registerCityEdge(citiesGraph &cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void registerHouse(citiesGraph &cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void registerPerson(citiesGraph &cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void showCities(citiesGraph cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void showHouses(citiesGraph cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
+
+void showPeople(citiesGraph cities) {
+  cout << endl;
+  cout << "Not implemented";
+  cout << endl;
+}
