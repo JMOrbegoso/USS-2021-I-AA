f7acefd46268c9daabb5ab9a827e56bef9d6bfd7
JM Orbegoso <dev@jmorbegoso.com>
2021-06-29 08:56:02 -0500

    Updated structFunctions.h

--- ClassroomExercises/Week-10_11/structFunctions.h
+++ ClassroomExercises/Week-10_11/structFunctions.h
@@ -5,33 +5,48 @@ using namespace std;
 
 // Factories
 
+personStruct buildPerson(string dni, string firstName, string lastName,
+                         float salary) {
+  personStruct person;
+
+  person.dni = dni;
+  person.firstName = firstName;
+  person.lastName = lastName;
+  person.salary = salary;
+
+  return person;
+}
+
+houseStruct buildHouse(string address, string owner,
+                       unsigned short floorsQuantity) {
+  houseStruct house;
+
+  house.address = address;
+  house.owner = owner;
+  house.floorsQuantity = floorsQuantity;
+
+  house.people.head = NULL;
+  house.people.length = 0;
+
+  return house;
+}
+
 cityStruct buildCity(string name, unsigned long inhabitantsQuantity) {
   cityStruct city;
 
   city.name = name;
-  city.inhabitantsQuantity = inhabitantsQuantity;
+
+  city.houses.head = NULL;
+  city.houses.length = 0;
 
   return city;
 }
 
-void buildEdge(nodo* aux_1, nodo* aux_2, edge* newEdge) {
-  edge* temp;
-  if (aux_1->adyacente == NULL) {
-    aux_1->adyacente = newEdge;
-    newEdge->destiny = aux_2;
-    cout << "Arista registrada";
-  } else {
-    newEdge->destiny = aux_2;
-    temp->next = newEdge;
-    cout << "Arista registrada";
-  }
-}
-
 // Init
 
-void collectionsInitialization(grafoStruct& grafo) {
-  grafo.nogoG = NULL;
-  grafo.edgeG = NULL;
+void collectionsInitialization(citiesGraph& cities) {
+  cities.node = NULL;
+  cities.edge = NULL;
 }
 
-void dataInitialization(grafoStruct& grafo) {}
+void dataInitialization(citiesGraph& cities) {}
