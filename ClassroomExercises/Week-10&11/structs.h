6d1daeed5c746ed987f3143366677c5167bdd8c5
JM Orbegoso <dev@jmorbegoso.com>
2021-06-28 22:33:20 -0500

    Update structs.h

--- ClassroomExercises/Week-10&11/structs.h
+++ ClassroomExercises/Week-10&11/structs.h
@@ -2,23 +2,62 @@
 
 using namespace std;
 
+// Person
+struct personStruct {
+  string dni;
+  string firstName;
+  string lastName;
+  float salary;
+};
+
+struct personNode {
+  personStruct person;
+  personNode *prev;
+  personNode *next;
+};
+
+struct peopleList {
+  personNode *head;
+  int length;
+}
+
+// House
+struct houseStruct {
+  string address;
+  string owner;
+  unsigned short floorsQuantity;
+  peopleList people;
+};
+
+struct houseNode {
+  houseStruct house;
+  houseNode *prev;
+  houseNode *next;
+};
+
+struct housesList {
+  houseNode *head;
+  int length;
+};
+
+// City
 struct cityStruct {
   string name;
-  unsigned long inhabitantsQuantity;
+  housesList houses;
 };
 
-struct nodo {
+struct cityNode {
   cityStruct city;
-  struct nodo *next;
-  struct edge *adyacente;
+  struct cityNode *next;
+  struct cityEdge *adjacent;
 };
 
-struct edge {
-  struct nodo *destiny;
-  struct edge *next;
+struct cityEdge {
+  struct cityNode *destiny;
+  struct cityEdge *next;
 };
 
-struct grafoStruct {
-  nodo *nogoG;
-  edge *edgeG;
+struct cityGraph {
+  cityNode *node;
+  cityEdge *edge;
 };
