1ed2105e4437f3c4aa3ad0d56e179d313c5fe2af
JM Orbegoso <dev@jmorbegoso.com>
2021-07-02 07:41:27 -0500

    Updated main.cpp in PAF

--- PAF/main.cpp
+++ PAF/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
   inicializacionDeColecciones(deltron);
   inicializacionDeData(deltron);
 
+  mostrarPresentacionEInstrucciones();
   menuPrincipal(deltron);
 
   return 0;
