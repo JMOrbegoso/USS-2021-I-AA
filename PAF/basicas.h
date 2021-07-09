b5451ae4473a59697a24a4948f77fc0d2f10a3a0
JM Orbegoso <dev@jmorbegoso.com>
2021-07-09 07:18:32 -0500

    Removed convertirAString methods

--- PAF/basicas.h
+++ PAF/basicas.h
@@ -12,9 +12,6 @@ string aMinuscula(string text) {
   return text;
 }
 
-string convertirAString(int number) { return to_string(number); }
-string convertirAString(float number) { return to_string(number); }
-
 char *toCharArray(string text) {
   char *charArray;
   string str_obj(text);
@@ -152,91 +149,6 @@ float leerNumeroDecimal(string requestMessage, string errorMessage, float min,
   }
 
   return amount;
-}
-
-float leerDinero(string mensaje, float min) {
-  string requestMessage, errorMessage;
-
-  requestMessage = mensaje + " (Mínimo S/" + convertirAString(min) + ")";
-  errorMessage = "Por favor, ingrese una cantidad mínima de S/" +
-                 convertirAString(min) + ".";
-
-  return leerNumeroDecimal(requestMessage, errorMessage, min);
-}
-
-float leerDinero(string mensaje, float min, float max) {
-  string requestMessage, errorMessage;
-
-  requestMessage = mensaje + " (Mínimo S/" + convertirAString(min) +
-                   " y máximo de S/" + convertirAString(max) + ")";
-  errorMessage = "Por favor, ingrese una cantidad mínima de S/" +
-                 convertirAString(min) + " y máxima de S/" +
-                 convertirAString(max) + ".";
-
-  return leerNumeroDecimal(requestMessage, errorMessage, min);
-}
-
-string leerFecha(string mensaje) {
-  string date;
-
-  // Year
-  int year = leerNumeroEntero(
-      "Año:", "Por favor ingrese un valor entre 1900 y 2050", 1900, 2050);
-
-  // Month
-  int selectedMonth;
-  string months[12];
-  months[0] = "Enero";
-  months[1] = "Febrero";
-  months[2] = "Marzo";
-  months[3] = "Abril";
-  months[4] = "Mayo";
-  months[5] = "Junio";
-  months[6] = "Julio";
-  months[7] = "Agosto";
-  months[8] = "Septiembre";
-  months[9] = "Octubre";
-  months[10] = "Noviembre";
-  months[11] = "Diciembre";
-
-  cout << endl << "Mes:" << endl << endl;
-
-  for (int i = 0; i < 12; i++) {
-    cout << "[" << (i + 1) << "] " << months[i] << endl;
-  }
-
-  cout << endl << "Introduzca la opción deseada:" << endl;
-  cin >> selectedMonth;
-  while (!(1 <= selectedMonth && selectedMonth <= 12)) {
-    cout << "Introdujo una opción inválida, por favor seleccione una opción "
-            "válida:"
-         << endl;
-    cin >> selectedMonth;
-  }
-  selectedMonth--;
-
-  int maxDays;
-  if (selectedMonth == 0 || selectedMonth == 2 || selectedMonth == 4 ||
-      selectedMonth == 6 || selectedMonth == 7 || selectedMonth == 9 ||
-      selectedMonth == 11) {
-    maxDays = 31;
-  } else if (selectedMonth == 3 || selectedMonth == 5 || selectedMonth == 8 ||
-             selectedMonth == 10) {
-    maxDays = 30;
-  } else if (selectedMonth == 1 && year % 4 == 0) {
-    maxDays = 29;
-  } else {
-    maxDays = 28;
-  }
-
-  // Day
-  int day = leerNumeroEntero(
-      "Día del mes:",
-      "Por favor ingrese un valor entre 1 y " + convertirAString(maxDays), 1,
-      maxDays);
-
-  return convertirAString(year) + "/" + months[selectedMonth] + "/" +
-         convertirAString(day);
 }
 
 void esperarMostrandoTexto(string mensaje, bool esperaLarga = false) {
