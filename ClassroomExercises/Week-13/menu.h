150ba8de2ff998c761f8890079c8caecbc456f1b
JM Orbegoso <dev@jmorbegoso.com>
2021-07-09 08:14:05 -0500

    Created menu.h in Week-13

---
+++ ClassroomExercises/Week-13/menu.h
@@ -0,0 +1,59 @@
+#include "menuOptions.h"
+
+using namespace std;
+
+int requestMenuOption() {
+  int selectedOption;
+
+  clearScreen();
+  showAppTitle();
+
+  cout << endl
+       << "Esta aplicación tiene las siguientes opciones:" << endl
+       << endl;
+
+  cout << "[1] Registrar estudiante" << endl;
+  cout << "[2] Mostrar estudiantes" << endl;
+  cout << "[3] Ordenar estudiantes" << endl;
+  cout << endl;
+  cout << "[0] Cerrar" << endl;
+
+  cout << endl << "Por favor ingrese una de las opciones:" << endl;
+  cin >> selectedOption;
+  while (!(0 <= selectedOption && selectedOption <= 11)) {
+    cout << "La opcion seleccionada fue inválida, por favor seleccione una "
+            "opcion válida:"
+         << endl;
+    cin >> selectedOption;
+  }
+  return selectedOption;
+}
+
+void mainMenu(studentsList &students) {
+  int selectedOption;
+
+  do {
+    selectedOption = requestMenuOption();
+
+    if (selectedOption != 0) {
+      switch (selectedOption) {
+        case 1:
+          registerStudent(students);
+          addDelay(1.5);
+          break;
+
+        case 2:
+          showStudents(students);
+          pauseProcess();
+          break;
+
+        case 3:
+          orderStudents(students);
+          addDelay(1.5);
+          break;
+      }
+    }
+  } while (!(selectedOption == 0));
+
+  cout << "Gracias por usar la app, tenga un buen día!";
+}
